95dcf4a62a3c4bed80f79286b23f4d7efe49b8a6
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2018-12-18 00:08:14 +0100

    Discretizing glucose level

--- mine.py
+++ mine.py
@@ -1,11 +1,31 @@
 import pandas as pd
+from enum import Enum
+
+class glucose(Enum):
+    low = 0
+    normal = 1
+    high = 2
+
+def discretizeGlucose(dataframe):
+    high_mask = dataframe["value"] > 200
+    normal_mask = (dataframe["value"] <= 200) & (dataframe["value"] >= 80)
+    low_mask = dataframe["value"] < 80
+    dataframe.loc[high_mask, "value"] = glucose.high
+    dataframe.loc[normal_mask, "value"] = glucose.normal
+    dataframe.loc[low_mask, "value"] = glucose.low
+    return dataframe
+
+def splitEventsStates(dataframe):
+    code_mask = (dataframe["code"] >= 48) & (dataframe["code"] <= 64)
+    events = dataframe[code_mask]
+    states = dataframe[~code_mask]
+    return events, states 
 
 def main():
     diabetes = pd.read_csv("data/diabetes/data-01", sep="\t", header = None, names=["date", "time", "code", "value"], parse_dates={"datetime": ["date", "time"]})
     diabetes = diabetes.sort_values(by = ["datetime"], ascending = True)
-    code_mask = (diabetes["code"] >= 48) & (diabetes["code"] <= 64)
-    events = diabetes[code_mask]
-    states = diabetes[~code_mask]
+    diabetes = discretizeGlucose(diabetes)
+    events, states = splitEventsStates(diabetes)
 
 if __name__ == "__main__":
     main()
