1728b487bff128beb8f3e7cca701cd42c75e3b2b
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2018-12-19 12:19:31 +0100

    Finding subsequences of change from low to normal in states

--- mine.py
+++ mine.py
@@ -21,6 +21,27 @@ def splitEventsStates(dataframe):
     states = dataframe[~code_mask]
     return events, states 
 
+# function that finds subsequences which contain a change of state from low to normal in a single day sequence of a patient
+def subsequencesInStatesLowToNormal(states_by_day):
+    subsequences = []
+    for _, group in states_by_day:   # _ is a day we group by, group is dataframe of measurements in that group
+        lowPosition = False
+        sequence = []
+        for _, measurement in group.iterrows(): # _ is an index of iterator, measurement is a row in our group
+            #already adding sequence, keep adding till glucose raises
+            if (lowPosition):
+                sequence.append(measurement)
+                if(measurement["value"] != glucose.low):
+                    lowPosition = False
+                    subsequences.append(sequence)
+            #not adding sequence yet, if glucose.low we start adding and clear any previous one (that didn't contain change)
+            else:
+                if(measurement["value"] == glucose.low):
+                    lowPosition = True
+                    sequence.clear()
+                    sequence.append(measurement)
+    return subsequences
+
 def main():
     diabetes = pd.read_csv("data/diabetes/data-01", sep="\t", header = None, names=["date", "time", "code", "value"], parse_dates=["date", "time"])
     diabetes["time"] = diabetes["time"].apply(lambda x: x.strftime("%H:%M:%S"))
@@ -28,8 +49,7 @@ def main():
     diabetes = discretizeGlucose(diabetes)
     events, states = splitEventsStates(diabetes)
     states_by_day = states.groupby("date")
-    for _, group in states_by_day:
-        print(group)
+    subsequencesLowToNormal = subsequencesInStatesLowToNormal(states_by_day)
 
 if __name__ == "__main__":
     main()
