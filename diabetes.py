a7d91b4a32226ba758b7cc31eb6c0fab41b502fb
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2019-03-25 23:30:05 +0100

    Code refactor - splitting files

---
+++ diabetes.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from enum import Enum
+
+class glucose(Enum):
+    low = 0
+    normal = 1
+    high = 2
+
+eventsDictionary = {
+    33: "Regular insulin dose",
+    34: "NPH insulin dose",
+    35: "UltraLente insulin dose",
+    48: "Unspecified blood glucose measurement",
+    57: "Unspecified blood glucose measurement",
+    58: "Pre-breakfast blood glucose measurement",
+    59: "Post-breakfast blood glucose measurement",
+    60: "Pre-lunch blood glucose measurement",
+    61: "Post-lunch blood glucose measurement",
+    62: "Pre-supper blood glucose measurement",
+    63: "Post-supper blood glucose measurement",
+    64: "Pre-snack blood glucose measurement",
+    65: "Hypoglycemic symptoms",
+    66: "Typical meal ingestion",
+    67: "More-than-usual meal ingestion",
+    68: "Less-than-usual meal ingestion",
+    69: "Typical exercise activity",
+    70: "More-than-usual exercise activity",
+    71: "Less-than-usual exercise activity",
+    72: "Unspecified special event"
+}
+
+def prepareDataDiabetes():
+    diabetes = pd.read_csv("data/diabetes_merged.csv", sep="\t", header = None, names=["date", "time", "code", "value"], parse_dates=["date", "time"])
+    diabetes["time"] = diabetes["time"].apply(lambda x: x.time())
+    diabetes["date"] = diabetes["date"].apply(lambda x: x.date())
+    diabetes = diabetes.sort_values(by = ["date", "time"], ascending = True)
+    diabetes = discretizeGlucose(diabetes)
+    events, states = splitEventsStatesDiabetes(diabetes)
+    return events, states
+
+def discretizeGlucose(dataframe):
+    dataframe["discret_val"] = ""
+    high_mask = dataframe["value"] > 200
+    normal_mask = (dataframe["value"] <= 200) & (dataframe["value"] >= 80)
+    low_mask = dataframe["value"] < 80
+    dataframe.loc[high_mask, "discret_val"] = glucose.high
+    dataframe.loc[normal_mask, "discret_val"] = glucose.normal
+    dataframe.loc[low_mask, "discret_val"] = glucose.low
+    return dataframe
+
+def splitEventsStatesDiabetes(dataframe):
+    code_mask = (dataframe["code"] >= 48) & (dataframe["code"] <= 64)
+    events = dataframe[~code_mask]
+    states = dataframe[code_mask]
+    return events, states 
--- mine.py
+++ mine.py
@@ -2,59 +2,7 @@ import pandas as pd
 from enum import Enum
 from prefixspan import PrefixSpan
 import numpy as np
-
-class glucose(Enum):
-    low = 0
-    normal = 1
-    high = 2
-
-eventsDictionary = {
-    33: "Regular insulin dose",
-    34: "NPH insulin dose",
-    35: "UltraLente insulin dose",
-    48: "Unspecified blood glucose measurement",
-    57: "Unspecified blood glucose measurement",
-    58: "Pre-breakfast blood glucose measurement",
-    59: "Post-breakfast blood glucose measurement",
-    60: "Pre-lunch blood glucose measurement",
-    61: "Post-lunch blood glucose measurement",
-    62: "Pre-supper blood glucose measurement",
-    63: "Post-supper blood glucose measurement",
-    64: "Pre-snack blood glucose measurement",
-    65: "Hypoglycemic symptoms",
-    66: "Typical meal ingestion",
-    67: "More-than-usual meal ingestion",
-    68: "Less-than-usual meal ingestion",
-    69: "Typical exercise activity",
-    70: "More-than-usual exercise activity",
-    71: "Less-than-usual exercise activity",
-    72: "Unspecified special event"
-}
-
-def discretizeGlucose(dataframe):
-    dataframe["discret_val"] = ""
-    high_mask = dataframe["value"] > 200
-    normal_mask = (dataframe["value"] <= 200) & (dataframe["value"] >= 80)
-    low_mask = dataframe["value"] < 80
-    dataframe.loc[high_mask, "discret_val"] = glucose.high
-    dataframe.loc[normal_mask, "discret_val"] = glucose.normal
-    dataframe.loc[low_mask, "discret_val"] = glucose.low
-    return dataframe
-
-def prepareData():
-    diabetes = pd.read_csv("data/diabetes_merged.csv", sep="\t", header = None, names=["date", "time", "code", "value"], parse_dates=["date", "time"])
-    diabetes["time"] = diabetes["time"].apply(lambda x: x.time())
-    diabetes["date"] = diabetes["date"].apply(lambda x: x.date())
-    diabetes = diabetes.sort_values(by = ["date", "time"], ascending = True)
-    diabetes = discretizeGlucose(diabetes)
-    events, states = splitEventsStates(diabetes)
-    return events, states
-
-def splitEventsStates(dataframe):
-    code_mask = (dataframe["code"] >= 48) & (dataframe["code"] <= 64)
-    events = dataframe[~code_mask]
-    states = dataframe[code_mask]
-    return events, states 
+from diabetes import prepareDataDiabetes, glucose, eventsDictionary
 
 # function that finds subsequences which contain a change of state from one to another in a single day sequence of a patient
 def subsequencesInStates(states_by_day, from_level, to_level):
@@ -176,7 +124,7 @@ def patternsToCSV(patterns, filename = "patterns.csv"):
     df.to_csv(filename, index = False, header = ["confidence", "support", "pattern"])
 
 def main():
-    events, states = prepareData()
+    events, states = prepareDataDiabetes()
 
     # mine positive patterns
     print("Getting events low to normal...")
